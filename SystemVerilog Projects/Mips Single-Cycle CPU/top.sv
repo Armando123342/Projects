bf9b501b2cc67d69c5a6819fe0280651f4fd8aa8
Armando Moreno <98512318+Armando123342@users.noreply.github.com>
2024-04-27 20:39:23 -0500

    Update top.sv

--- SystemVerilog Projects/Mips Single-Cycle CPU/top.sv
+++ SystemVerilog Projects/Mips Single-Cycle CPU/top.sv
@@ -1,20 +1,16 @@
-//------------------------------------------------
-// Top level system including MIPS and memories
-//------------------------------------------------
-
 module top(input  logic        clk, reset, 
            output logic [31:0] writedata, dataadr, 
            output logic        memwrite,
 	   output logic [31:0] pc, instr, readdata);
 
-  //logic [31:0] pc, instr, readdata;
-  
-  // instantiate processor and memories
 
+  // The core of the code
   mips mips(clk, reset, pc, instr, memwrite, dataadr, writedata, readdata);
 
+  // Instruction Memory
   imem imem(pc[7:2], instr);
 
+  // Data Memory
   dmem dmem(clk, memwrite, dataadr, writedata, readdata);
 
 endmodule
