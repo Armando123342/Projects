b55f66ffa168eb306572000e2db98b50bd8ef6be
Armando Moreno <98512318+Armando123342@users.noreply.github.com>
2024-04-27 23:46:00 -0500

    Update testbench_C.sv

--- SystemVerilog Projects/Mips Single-Cycle CPU/testbench_C.sv
+++ SystemVerilog Projects/Mips Single-Cycle CPU/testbench_C.sv
@@ -1,5 +1,4 @@
-// Example testbench for MIPS processor
-
+// Testbench for memfile_c.dat
 module testbench();
 
   logic        reset;
@@ -29,11 +28,10 @@ module testbench();
   always@(negedge clk)
     begin
       if(memwrite) begin
-        if(aluout === 32'h00000010 & writedata === 32'hfffffffa) begin // Part A: 54 7, Part B: 14 1c, Part C: 32'h00000010 & 32'hfffffffa
+        if(aluout === 16 & writedata === -6) begin 
           $display("Simulation succeeded");
           $stop;
-        end else if (aluout !== 32'h14 & aluout !== 32'h18 & aluout !== 32'h1c & aluout !== 32'h20 & aluout !== 32'h24 & aluout !== 32'h28) begin // Part A: 80, Part B: 34 20 1c 18, Part C: 14 18 1c 20 24 28
-          $display("Simulation failed");
+        end else if (aluout !== 32'h14 & aluout !== 32'h18 & aluout !== 32'h1c & aluout !== 32'h20 & aluout !== 32'h24 & aluout !== 32'h28) begin
           $stop;
         end
       end
