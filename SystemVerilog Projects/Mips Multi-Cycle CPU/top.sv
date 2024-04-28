bfd15e5dd80919bbdcf415d1f4f95cfbc1d1bcce
Armando Moreno <98512318+Armando123342@users.noreply.github.com>
2024-04-28 00:14:56 -0500

    Update top.sv

--- SystemVerilog Projects/Mips Multi-Cycle CPU/top.sv
+++ SystemVerilog Projects/Mips Multi-Cycle CPU/top.sv
@@ -1,9 +1,23 @@
-//-------------------------------------------------------
-// mipsmulti.v
-// David_Harris@hmc.edu 8 November 2005
-// Update to SystemVerilog 17 Nov 2010 DMH
-// Multicycle MIPS processor
-//------------------------------------------------
+// Mips Multi-Cycle CPU
+module top(input logic clk, reset,
+ 	   output logic [5:0]  op, funct,
+           output logic [3:0]  state,
+           output logic [15:0] controlword,
+           output logic [31:0] pc,
+	   output logic [31:0] aluresult, adr, writedata,
+	   output logic        memwrite);
+
+  logic [31:0] readdata;
+
+  // microprocessor (control & datapath)
+  mips mips(clk, reset, adr, writedata, memwrite, readdata, 
+            op, funct, state, controlword, pc, aluresult);
+
+  // memory
+  mem mem(clk, memwrite, adr, writedata, readdata);
+
+endmodule
+
 
 module mips(input  logic        clk, reset,
             output logic [31:0] adr, writedata,
@@ -55,12 +69,7 @@ module controller(input  logic        clk, reset,
              alusrcb, pcsrc, aluop, ble, state, controlword);
   aludec  ad(funct, aluop, alucontrol);
 
-  // ADD CODE HERE
-  //assign pcen = pcwrite | (branch & zero);
   assign pcen = (branch ? zero : (ble & (zero | alur))) | pcwrite;
-  // Add combinational logic (i.e. an assign statement) 
-  // to produce the PCEn signal (pcen) from the branch, 
-  // zero, and pcwrite signals
  
 endmodule
 
@@ -104,10 +113,6 @@ module maindec(input  logic        clk, reset,
     if(reset) state <= FETCH;
     else state <= nextstate;
 
-  // ADD CODE HERE
-  // Finish entering the next state logic below.  We've completed the first 
-  // two states, FETCH and DECODE, for you.
-
   // next state logic
   always_comb
     case(state)
@@ -146,9 +151,6 @@ module maindec(input  logic        clk, reset,
           alusrca, branch, iord, memtoreg, regdst,
           alusrcb, pcsrc, aluop} = controls;
 
-  // ADD CODE HERE
-  // Finish entering the output logic below.  We've entered the
-  // output logic for the first two states, S0 and S1, for you.
   always_comb
     case(state)
       FETCH:   controls <= 16'h5010;
@@ -192,15 +194,7 @@ module aludec(input  logic [5:0] funct,
   
 endmodule
 
-
-// Complete the datapath module below for Lab 11.
-// You do not need to complete this module for Lab 10
-
-// The datapath unit is a structural verilog module.  That is,
-// it is composed of instances of its sub-modules.  For example,
-// the instruction register is instantiated as a 32-bit flopenr.
-// The other submodules are likewise instantiated.
-
+// The datapath unit is a structural verilog module.
 module datapath(input  logic        clk, reset,
                 input  logic        pcen, irwrite, regwrite,
                 input  logic        alusrca, iord, memtoreg, regdst,
@@ -212,8 +206,6 @@ module datapath(input  logic        clk, reset,
                 input  logic [31:0] readdata,
 		output logic [31:0] aluresult,
                 output logic [31:0] pc);
-
-  // Below are the internal signals of the datapath module.
 
   logic [4:0]  writereg;
   logic [31:0] pcnext;
@@ -228,20 +220,6 @@ module datapath(input  logic        clk, reset,
   // op and funct fields to controller
   assign op = instr[31:26];
   assign funct = instr[5:0];
-
-  // Your datapath hardware goes below.  Instantiate each of the submodules
-  // that you need.  Remember that alu's, mux's and various other 
-  // versions of parameterizable modules are available in mipsparts.sv
-  // from Lab 9. You'll likely want to include this verilog file in your
-  // simulation.
-
-  // We've included parameterizable 3:1 and 4:1 muxes below for your use.
-
-  // Remember to give your instantiated modules applicable names
-  // such as pcreg (PC register), wdmux (Write Data Mux), etc.
-  // so it's easier to understand.
-
-  // ADD CODE HERE
 
   // datapath
 
@@ -279,42 +257,6 @@ assign y = {a[25:0], 2'b00};
 endmodule
 
 
-module readreg (input logic clk, reset,
-                input logic [31:0] rd1, rd2,
-		output logic [31:0] a, b);
-
-  flopr #(32) areg(clk, reset, rd1, a);
-  flopr #(32) breg(clk, reset, rd2, b);
-
-endmodule
-	       
-
-module mux3 #(parameter WIDTH = 8)
-             (input  logic [WIDTH-1:0] d0, d1, d2,
-              input  logic [1:0]       s, 
-              output logic [WIDTH-1:0] y);
-
-  assign #1 y = s[1] ? d2 : (s[0] ? d1 : d0); 
-
-endmodule
-
-
-module mux4 #(parameter WIDTH = 8)
-             (input  logic [WIDTH-1:0] d0, d1, d2, d3,
-              input  logic [1:0]       s, 
-              output logic [WIDTH-1:0] y);
-
-   always_comb
-      case(s)
-         2'b00: y <= d0;
-         2'b01: y <= d1;
-         2'b10: y <= d2;
-         2'b11: y <= d3;
-      endcase
-
-endmodule
-
-
 module mem(input logic clk, we,
  	   input logic [31:0] a, wd,
  	   output logic [31:0] rd);
@@ -333,26 +275,6 @@ module mem(input logic clk, we,
 
   always_ff @(posedge clk)
     if (we) RAM[a[31:2]] <= wd;
-
-endmodule
-
-
-module top(input logic clk, reset,
- 	   output logic [5:0]  op, funct,
-           output logic [3:0]  state,
-           output logic [15:0] controlword,
-           output logic [31:0] pc,
-	   output logic [31:0] aluresult, adr, writedata,
-	   output logic        memwrite);
-
-  logic [31:0] readdata;
-
-  // microprocessor (control & datapath)
-  mips mips(clk, reset, adr, writedata, memwrite, readdata, 
-            op, funct, state, controlword, pc, aluresult);
-
-  // memory
-  mem mem(clk, memwrite, adr, writedata, readdata);
 
 endmodule
 
